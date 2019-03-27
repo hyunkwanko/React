84a2541b662c29c077f4194a8f171a0ee01a4986
hyunkwanko <cjdshrwkd11@gmail.com>
2019-03-27 11:47:49 +0900

    zzggzgz

--- WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -17,6 +17,6 @@ namespace WindowsFormsApplication1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-        }
+        }//dds
     }
 }
